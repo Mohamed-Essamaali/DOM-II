13a368d24c19cc4fcc41f55f07e7f239c77ec536
Mohamed-Essamaali <mohamed.essamaali@gmail.com>
2020-02-17 22:43:01 -0500

    added wheel resize select

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+
 // Your code goes here
 
 const links = document.querySelectorAll('.nav-link');
@@ -31,43 +33,74 @@ const funBus = document.querySelector('.home');
 
 document.addEventListener('keydown',(e)=>{ 
     funBus.style.backgroundImage = "linear-gradient(#f2dc68,#4ffef8)";
-   
+   funBus.style.color = "#454545";
 });
+
 //double click
 const nav = document.querySelector('.nav-container');
-document.addEventListener('dblclick',(e)=>{ 
+nav.addEventListener('dblclick',(e)=>{ 
     e.target.style.backgroundImage = "linear-gradient(#f2dc68,#4ffef8)";
    
 });
 
 //click
+
+const boat21 = document.querySelector('.boatImg');
+
+   boat21.addEventListener('click',()=>{
+
+      let start = Date.now();
+      let timer = setInterval(()=>{
+        let timePassed = Date.now()-start;
+           
+            if (timePassed>3000){
+            clearInterval(timer) //reset timer
+            }else
+            boat21.style.width = timePassed/5+'px';
+        }); // interval method
+        
+     }); // event listener method
+
 const navContainer = document.querySelector('.nav');
 
-document.addEventListener('click',e=>{
-    navContainer.style.width='85%';
-    navContainer.style.backgroundColor = "#cbf5db";
-    navContainer.style.padding='2%';
+navContainer.addEventListener('click',(e)=>{
+    e.target.style.width='85%';
+    e.target.style.backgroundColor = "#cbf5db";
+    e.target.style.padding='2%';
  });
 
 //wheel
 const destination = document.querySelector('.intro img');
 
-document.addEventListener('mouseenter',()=>{
+destination.addEventListener('wheel',(e)=>{
+    e.target.style.transform ="scale(0.8)";
+    // e.target.style.transform = " translate(10px,200px)";
+         
+        e.target.style.border="2px dashed #4a4a4a";
+        
+        });
+// resize
+
+        window.addEventListener('resize',()=>{
+            destination.src="https://images.unsplash.com/photo-1501393091915-82f0cbd8f338?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60";
+            destination.style.width= "100%";
+        });
+ 
+// select
+
+const introduction = document.querySelector('.intro p');
+    introduction.addEventListener('select', (event)=>{
+
+    const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
+       
+   alert(`You selected: ${selection}`);
+  
+});
+
+   
+
+       
     
-    destination.style.transform = "rotate(15deg) translate(10px,10px)"
-
-    document.addEventListener('mouseout',()=>{
-        destination.style.transform = "rotate(0deg)"
-    })
-})
-
-
-document.addEventListener('wheel',()=>{
-    
-          destination.style.transform = "scale(1.2)";
-    });
-
-
 
 
 
