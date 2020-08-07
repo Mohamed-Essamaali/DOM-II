b6ec7b056bd6f61c6f17382373ac250d167c5cae
Mohamed essamaali <mohamed.essamaali@gmail.com>
2020-08-06 23:06:06 -0400

    Added MouseEnter out and dblclick event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+const mainNavigation = document.querySelector('.main-navigation');
+mainNavigation.addEventListener('mouseover',(e)=>{
+    e.stopPropagation();
+    mainNavigation.style.backgroundColor='hotpink'
+})
+mainNavigation.addEventListener('mouseout',(e)=>{
+    // e.stopPropagation();
+    mainNavigation.style.backgroundColor='white'
+})
+const headerImg = document.querySelector('.intro img')
+window.addEventListener('resize',e=>{
+        headerImg.style.width='300px'
+   
+  })
+
+  const contentSection = document.querySelectorAll('.content-section')
+
+  contentSection.forEach(el=>{
+      el.addEventListener('dblclick',e=>{
+        el.style.backgroundColor='black';
+        el.style.color='white'
+      })
+      
+  })
